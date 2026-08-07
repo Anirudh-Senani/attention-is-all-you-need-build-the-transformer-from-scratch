a4cfae73b7c9efc9ba68654f335097e3afef5a81
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-07 13:46:23 +0530

    Step 9: build_position_index_column

--- model.py
+++ model.py
@@ -75,8 +75,13 @@ def compute_positional_div_term(d_model):
     # TODO: return a 1D FloatTensor of length d_model // 2 holding the sinusoidal frequency divisors
     return 10000**(-(2 * torch.arange(d_model//2)/d_model))
 
-# Step 9 - build_position_index_column (not yet solved)
-# TODO: implement
+# Step 9 - build_position_index_column
+import torch
+
+def build_position_index_column(max_len):
+    """Return a (max_len, 1) float tensor of [0, 1, ..., max_len-1]."""
+    # TODO: build a column vector of position indices from 0 to max_len-1
+    return torch.arange(max_len, dtype=torch.float32)[:, None]
 
 # Step 10 - fill_even_indices_with_sin (not yet solved)
 # TODO: implement
