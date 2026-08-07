4052b45378df8d3ba2e457c86bddc4286f79c2b3
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-07 17:25:27 +0530

    Step 28: split_qkv_into_heads

--- model.py
+++ model.py
@@ -249,8 +249,18 @@ def project_to_query_key_value(x, w_q, b_q, w_k, b_k, w_v, b_v):
     value = apply_linear_projection(x, w_v, b_v)
     return query, key, value
 
-# Step 28 - split_qkv_into_heads (not yet solved)
-# TODO: implement
+# Step 28 - split_qkv_into_heads
+import torch
+
+def split_qkv_into_heads(q, k, v, num_heads):
+    # TODO: split each of q, k, v into (B, num_heads, L, d_k) and return as a tuple
+    q_heads = split_last_dim_into_heads(q, num_heads)
+    q_heads = transpose_heads_before_sequence(q_heads)
+    k_heads = split_last_dim_into_heads(k, num_heads)
+    k_heads = transpose_heads_before_sequence(k_heads)
+    v_heads = split_last_dim_into_heads(v, num_heads)
+    v_heads = transpose_heads_before_sequence(v_heads)
+    return q_heads, k_heads, v_heads
 
 # Step 29 - multi_head_scaled_dot_product_attention (not yet solved)
 # TODO: implement
