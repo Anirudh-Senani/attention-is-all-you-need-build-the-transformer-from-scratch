e515fe7bffcfa96aca2dd53552288b99bd551f10
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-08 21:26:18 +0530

    Step 78: append_tokens_to_beam_sequences

--- model.py
+++ model.py
@@ -857,8 +857,12 @@ def select_top_k_candidates(candidate_scores, k):
         scores=scores
     )
 
-# Step 78 - append_tokens_to_beam_sequences (not yet solved)
-# TODO: implement
+# Step 78 - append_tokens_to_beam_sequences
+import torch
+
+def append_tokens_to_beam_sequences(beam_sequences, beam_indices, token_ids):
+    # TODO: gather parent beam rows and append the new token ids as the last column
+    return torch.cat((beam_sequences[beam_indices], token_ids.unsqueeze(1)), dim=1)
 
 # Step 79 - mark_finished_beams (not yet solved)
 # TODO: implement
